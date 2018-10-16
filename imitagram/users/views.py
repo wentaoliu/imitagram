ddaaab8125106c2ba50f321014f593b2498132cc
Wentao Liu <liuwnt@gmail.com>
2018-10-16 14:30:13 +1100

    fix a bug in user model

--- imitagram/users/views.py
+++ imitagram/users/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from imitagram.users.models import User
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
