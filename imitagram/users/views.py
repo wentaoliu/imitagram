7c01efa152fc3f9759d7d722b48e835c97c009e5
Wentao Liu <liuwnt@gmail.com>
2018-10-20 16:22:17 +1100

    fix a bug

--- imitagram/users/views.py
+++ imitagram/users/views.py
@@ -34,7 +34,8 @@ def self_suggest(request):
     for f in followings:
         res = Relationship.objects.filter(source=f.sink)
         if res.count() > 0:
-            suggests += [r.sink for r in res if r.sink is not request.user]
+            suggests += [r.sink for r in res if r.sink != request.user]
+    suggests.remove(request.user)
     serializer = UserSerializer(suggests, many=True)
     return Response(serializer.data)
 
